e8e31ba402f863721982d8e374bc9547a14f2a4f
oriba <ori303@gmail.com>
2015-04-17 11:03:27 +0300

    New DAL layer with DATASET and Interface

--- Coupons/DAL/CDAL.cs
+++ Coupons/DAL/CDAL.cs
@@ -8,6 +8,27 @@ namespace DAL
 {
     public class CDAL
     {
-        
+        public void test2(){
+            //creating admin adapters
+            CouponsDataSetTableAdapters.AdminTableAdapter adminAdapter = new CouponsDataSetTableAdapters.AdminTableAdapter();
+            //testing SELECT query
+            CouponsDataSet.AdminDataTable adminTable = adminAdapter.SelectByKey("303013114");
+            if (adminTable.Rows.Count == 1)
+            {
+                Console.WriteLine("found");
+            }
+            else
+            {
+                Console.WriteLine("not found");
+            }
+        }
+        public void test1(){
+            //creating admin adapters
+            CouponsDataSetTableAdapters.AdminTableAdapter adminAdapter = new CouponsDataSetTableAdapters.AdminTableAdapter();
+            //testing INSERT query
+            adminAdapter.InsertAdmin("303013114", "dima", "dimitry", "dima@dimahoo.com", "0542222333", "1234");
+
+        }
+   
     }
 }
---
+++ Coupons/DAL/IDAL.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    interface IDAL
+    {
+        //Admin
+        public bool AdminExistsByKey(string id);
+        public void AdminInsert(string id, string fname, string lname, string email, string phone, string password);
+        public void AdminUpdateByKey(string whereID, string fname, string lname, string email, string phone, string password);
+        public void AdminDeleteByKey(string whereID);
+        //Customer
+        public bool CustomerExistsByKey(string id);
+        public void CustomerInsert(string id, string fname, string lname, string email, string phone, int age, string password);
+        public void CustomerUpdateByKey(string whereID, string fname, string lname, string email, string phone, int age, string password);
+        public void CustomerDeleteByKey(string whereID);
+    }
+}
