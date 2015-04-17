3e7c57abef76954941f7dd0e7c194ff590c0b3f5
aviramad <aviram.adiri@hotmail.com>
2015-04-17 12:54:26 +0300

    finish ERD, create new project : TestUnit

---
+++ Coupons/TestUnit/DBTester.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using DAL;
+
+namespace TestUnit
+{
+    [TestFixture]
+    public class DBTester
+    {
+        [Test]
+        public void addAdmin()
+        {
+            int x = 7;
+            int y = 7;
+            Assert.AreEqual(x, y);
+        }
+
+        [Test]
+        public void positiveTest()
+        {
+            int x = 7;
+            int y = 7;
+            Assert.AreEqual(x, y);
+        }
+
+        [Test]
+        public void negativeTest()
+        {
+            int x = 7;
+            int y = 8;
+            Assert.AreNotEqual(x, y);
+        }
+    }
+}
